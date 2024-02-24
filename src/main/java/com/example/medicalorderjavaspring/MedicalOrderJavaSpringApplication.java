a0d4b7e4a57aa889c47bb82a22b4614f5f3b7ec5
hasdan <230547@astait.edu.kz>
2024-02-24 21:19:51 +0600

    CREATION

---
+++ src/main/java/com/example/medicalorderjavaspring/MedicalOrderJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicalorderjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalOrderJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalOrderJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medicalorderjavaspring/MedicalOrderJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalorderjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalOrderJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
