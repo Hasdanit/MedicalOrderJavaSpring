9dacd2543c097c4a4904b6ef3d1024a8cc664694
gazizatanirbergen <230553@astanait.edu.kz>
2024-02-27 14:00:24 +0600

    Добавила Остальное

--- src/main/java/com/example/medicalorderjavaspring/controllers/PatientController.java
+++ src/main/java/com/example/medicalorderjavaspring/controllers/PatientController.java
@@ -1,4 +1,22 @@
 package com.example.medicalorderjavaspring.controllers;
 
+import com.example.medicalorderjavaspring.models.Person;
+import com.example.medicalorderjavaspring.services.PersonService;
+import com.example.medicalorderjavaspring.services.interfaces.PersonServiceInterface;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("person")
+
+
 public class PatientController {
+
 }
--- src/main/java/com/example/medicalorderjavaspring/services/PersonService.java
+++ src/main/java/com/example/medicalorderjavaspring/services/PersonService.java
@@ -2,16 +2,16 @@ package com.example.medicalorderjavaspring.services;
 
 import com.example.medicalorderjavaspring.models.Person;
 import com.example.medicalorderjavaspring.services.interfaces.PersonServiceInterface;
-import com.example.medicalorderjavaspring.repositories.UserRepositoryInterface;
+import com.example.medicalorderjavaspring.repositories.PersonRepositoryInterface;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PersonService implements PersonServiceInterface {
-    private final PersonServiceInterface repo;
+    private final PersonRepositoryInterface repo;
 
-    public PersonService(PersonServiceInterface repo) {
+    public PersonService(PersonRepositoryInterface repo) {
         this.repo = repo;
     }
 
@@ -34,5 +34,4 @@ public class PersonService implements PersonServiceInterface {
     public List<Person> getBySurname(String surname) {
         return repo.findBySurname(surname);
     }
-
 }
