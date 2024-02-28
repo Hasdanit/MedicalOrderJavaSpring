a578d1a42546bcd47355c6e6727d5a195855b641
gazizatan <153696424+gazizatan@users.noreply.github.com>
2024-02-28 07:28:28 +0600

    aaaa ya ne spala no dumayu comments ne nuzhny ya delayu rabotu

--- src/main/java/com/example/medicalorderjavaspring/Repositories/TransactionRepositoryInterface.java
+++ src/main/java/com/example/medicalorderjavaspring/Repositories/TransactionRepositoryInterface.java
@@ -1,4 +1,5 @@
 package com.example.medicalorderjavaspring.Repositories;
 
 public interface TransactionRepositoryInterface {
+
 }
--- src/main/java/com/example/medicalorderjavaspring/Services/interfaces/TransactionServiceInterface.java
+++ src/main/java/com/example/medicalorderjavaspring/Services/interfaces/TransactionServiceInterface.java
@@ -1,4 +1,17 @@
 package com.example.medicalorderjavaspring.Services.interfaces;
 
+import com.example.medicalorderjavaspring.Models.Transaction;
+
+import java.util.Date;
+import java.util.List;
+
 public interface TransactionServiceInterface {
+    Transaction create(Transaction transaction);
+    Transaction getById(int id);
+    List<Transaction> getAll();
+    Transaction getByDiscription(String description);
+    Transaction getByAmount(double amount);
+    List<Transaction> getByDate(Date date);
+
+
 }
