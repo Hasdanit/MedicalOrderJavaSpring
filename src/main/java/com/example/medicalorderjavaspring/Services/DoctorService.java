a8f9c6c393571c8aa9d40cfe65548a17962f9129
Malayev Yerkanat <153696190+yerkanat7@users.noreply.github.com>
2024-02-28 08:29:00 +0600

    Update DoctorService.java

--- src/main/java/com/example/medicalorderjavaspring/Services/DoctorService.java
+++ src/main/java/com/example/medicalorderjavaspring/Services/DoctorService.java
@@ -1,4 +1,32 @@
 package com.example.medicalorderjavaspring.Services;
 
-public class DoctorService {
+import com.example.medicalorderjavaspring.Models.Doctor;
+import com.example.medicalorderjavaspring.Repositories.DoctorRepositoryInterface;
+import com.example.medicalorderjavaspring.Services.interfaces.DoctorServiceInterface;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DoctorService implements DoctorServiceInterface {
+    private final DoctorRepositoryInterface repo;
+
+    public DoctorService(DoctorRepositoryInterface repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public List<Doctor> getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Doctor create(Doctor doctor) {
+        return repo.save(doctor);
+    }
+
+    @Override
+    public Doctor getById(int id) {
+        return repo.findById(id).orElse(null);
+    }
 }
