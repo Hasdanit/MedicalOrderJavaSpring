600f0737807202846a9878ad12d909f988ec3a69
gazizatan <153696424+gazizatan@users.noreply.github.com>
2024-02-28 07:23:44 +0600

    дополнила пациента

--- src/main/java/com/example/medicalorderjavaspring/Services/interfaces/PatientServiceInterface.java
+++ src/main/java/com/example/medicalorderjavaspring/Services/interfaces/PatientServiceInterface.java
@@ -1,4 +1,20 @@
 package com.example.medicalorderjavaspring.Services.interfaces;
 
+import com.example.medicalorderjavaspring.Models.Patient;
+
+import java.util.List;
+
 public interface PatientServiceInterface {
+    List<Patient> getAll();
+
+    Patient create(Patient patient);
+
+    Patient getById(int id);
+
+    Patient getByDiagnosis(String diagnosis);
+    Patient getByIsOnReecord(boolean isOnReecord);
+    Patient getByBloodType(String bloodType);
+    Patient getByIsVaccinated(boolean isVaccinated);
+    Patient getByChronicDiseases(String chronicDiseases);
+    Patient getByIsInsurance(boolean isInsurance);
 }
